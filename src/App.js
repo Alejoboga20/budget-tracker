211a1b334e3f14c6b0fe4a62118ab7499b6b6000
Alejo Boga <60139228+Alejoboga20@users.noreply.github.com>
2021-08-31 15:25:28 -0300

    Creating main card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import Details from './components/Details/Details';
 import useStyles from './styles';
+import Main from './components/Main/Main';
 
 export const App = () => {
 	const classes = useStyles();
@@ -20,7 +21,7 @@ export const App = () => {
 					<Details title='Income' />
 				</Grid>
 				<Grid item xs={12} sm={4}>
-					Main
+					<Main />
 				</Grid>
 				<Grid item xs={12} sm={4}>
 					<Details title='Expense' />
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,39 @@
+import {
+	Card,
+	CardContent,
+	CardHeader,
+	Divider,
+	Grid,
+	Typography,
+} from '@material-ui/core';
 import React from 'react';
+import useStyles from './styles';
 
 const Main = () => {
-	return <div></div>;
+	const classes = useStyles();
+
+	return (
+		<Card className={classes.root}>
+			<CardHeader title='Expense Tracker' subheader='Powered by Speechly' />
+			<CardContent>
+				<Typography align='center' variant='h5'>
+					Total Balance $100
+				</Typography>
+				<Typography
+					variant='subtitle 1'
+					style={{ lineHeight: '1.5rem', marginTop: '20px' }}
+				>
+					Try Saying: Add income for $100 in Category Salary for Monday
+				</Typography>
+				<Divider />
+				<CardContent className={classes.CardContent}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}></Grid>
+					</Grid>
+				</CardContent>
+			</CardContent>
+		</Card>
+	);
 };
 
 export default Main;
